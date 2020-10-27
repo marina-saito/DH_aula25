4878fdb9fe5219b43ef1da45293652d9b6fdf5f2
Marina <marina.ssaito@gmail.com>
2020-10-26 22:46:25 -0300

    Exercicio 6 continuaçao

--- app/src/main/java/com/example/snackbar/ui/MainActivity.kt
+++ app/src/main/java/com/example/snackbar/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.snackbar.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -68,7 +69,7 @@ class MainActivity : AppCompatActivity(), ContractMainActivity {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.profile -> {
-                Toast.makeText(this, "PERFIL DE USUARIO", Toast.LENGTH_SHORT).show()
+                callProfile()
                 return true
             }
             else -> super.onOptionsItemSelected(item)
@@ -138,6 +139,26 @@ class MainActivity : AppCompatActivity(), ContractMainActivity {
 
     fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun callProfile(){
+        var intent = Intent(this, ProfileActivity::class.java)
+        intent.putExtra("bundle", Bundle().apply {
+            putString("nome", "")
+            putInt("idade", 0)
+            putInt("peso", 0)
+            putString("status", "Solteiro")
+        })
+
+        val bundle:Bundle?=intent.extras
+        if (bundle != null) {
+            val nome = bundle.getString("nome")
+            val peso = bundle.getDouble("peso")
+            val idade = bundle.getInt("idade")
+            val titular = bundle.getBoolean("titular")
+            Toast.makeText(this, bundle.toString(), Toast.LENGTH_SHORT).show() }
+
+        startActivity(intent)
     }
 
 }
